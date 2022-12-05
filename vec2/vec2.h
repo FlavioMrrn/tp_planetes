14a34f42e47af6e3140c1164a57f91ffe118892f
FlavioMrrn <flavio.mrrn@gmail.com>
2022-12-05 10:42:00 +0100

    ok

--- vec2/vec2.h
+++ vec2/vec2.h
@@ -16,5 +16,5 @@ vec2 add(vec2 *v1, vec2 *v2);
 vec2 sub(vec2 *v1, vec2 *v2);
 double dot(vec2 *v1, vec2 *v2);
 double norme(vec2 *v1);
-vec2 multiplication(vec2 *v1, float *scalaire);
+vec2 multiplication(vec2 *v1, float scalaire);
 vec2 normalisation(vec2 *v1);
